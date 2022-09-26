40a333e1cc8afee77fa6daea492be4095b5193fa
macadamiaboy <elknumber1@yandex.com>
2022-09-26 15:54:01 +0300

    Corrections made

--- js/main.js
+++ js/main.js
@@ -2,17 +2,18 @@
 //В функциях random() и floor() разбирался на https://developer.mozilla.org/
 
 const customRandom = function(from, to) {
+  if (from < 0 || to < 0) {
+    throw new Error('Arguments should be positive');
+  }
   if (from > to) {
-    const third = from;
-    from = to;
-    to = third;
+    [from, to] = [to, from];
   } else if (from === to) {
     return from;
   }
   const random = from + Math.random() * (to + 1 - from);
   return Math.floor(random);
 };
-(customRandom(4, 8));
+customRandom(4, 8);
 
 const lengthCheck = (stringToCheck, maxLength) => stringToCheck.length <= maxLength;
 lengthCheck('allowed', 7);
